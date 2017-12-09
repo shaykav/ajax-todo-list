91b93eda56d68d0c956dc1f48f7687fec95d47ec
Shaila Kavrakova <shaila.kavrakova@gmail.com>
2017-12-09 01:30:16 -0500

    tasks scaffold and create forms

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,8 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+
+$(document).on('turbolinks:load', function(){
+  console.log('document is ready', new Date());
+  $('#task_name').focus();
+});
